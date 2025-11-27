bd067de4bb9d005a1a083c4ecbc2eec56429c648
T3slaVortex9X <laiqh283@gmail.com>
2025-11-27 03:07:31 -0500

    update

--- calc.py
+++ calc.py
@@ -1,54 +1,63 @@
 # Perform simple arithmetic encoded in an input string:
 # '1 + 2' -> 3, or '1 - 2' -> -1.
-def add(x, y):
-    return x+y
-def subtract(x, y):
-    return x-y
-def multiply(x, y):
-    return x*y
-def divide(x, y):
-    return x/y
-def power(x, y):
-    return x**y
+
+
+## Simple Calculator
+
+
+
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    return a / b
+def power(a, b):
+    return a ** b
 
 def calculator():
     while True:
-        print("___Simple Python Calculator___")
-        print("Selecet and option")
+
+        print("---Simple Python Calculator---")
+        print("Seclect Operation")
         print("1. Add")
-        print("2. Subtract")
+        print("2. subtract")
         print("3. Multiply")
-        print("4. Divide")
-        print("5. Power")
+        print("4. divide")
+        print("5. power")
         print("6. Exit")
-        choice = input("Select one of the above option")
+
+        choice = input("Enter a number (1-5):")
         if choice == '6':
-            print("Closing the app...\n")
-            print(".......\n")
+            print("Good bye")
             break
-        if choice not in ['1', '2', '3', '4', '5', '6']:
-            print("Invalid operation")
+
+        if choice not in ['1', '2', '3', '4', '5']:
+
+            print("Invalid Error!")
             return
-    try:
-        num1 = float(input("Enter your first number:"))
-        num2 = float(input("Enter your second number:"))
-    except ValueError:
-                     print("Invalid Error!")
-                     return
+
+        try:
+
+           num1 = float(input("Enter your first number: "))
+           num2 = float(input("Enter your second number: "))
+        except ValueError:
+          print("Invalid numbers Entered")
+          return
+
         if choice == '1':
-                     print(f"Result: add{num1, num2}")
+
+           print(f"Result: {add(num1, num2)}")
         elif choice == '2':
-                     print(f"Result: subtract{num1, num2}")
+           print(f"Result: {subtract(num1, num2)}")
         elif choice == '3':
-                     print(f"Result: multiply{num1, num2}")
+            print(f"Result: {multiply(num1, num2)}")
         elif choice == '4':
-                     print(f"Result: divide{num1, num2}")
+            print(f"Result: {divide(num1, num2)}")
         elif choice == '5':
-                      print(f"Result: power{num1, num2}")
+            print(f"Result: {power(num1, num2)}")
+
 if __name__ == "__main__":
-                     culculator()
-                    
-
-
-
-
+    calculator()
